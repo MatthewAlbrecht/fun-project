104e277d76debd3136a6b7741d9072ed78d5f66c
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2022-10-06 22:34:51 -0400

    add barchart and reset button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useMemo } from "react";
 import "./App.css";
 import useRequest from "./useRequest";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
 function App() {
   const { data, loading, error } = useRequest();
-  const [filters, setFilters] = React.useState({
-    year: null,
-    quarter: null,
-    homeOwnership: null,
-    term: null
-  });
+  const [filters, setFilters] = React.useState({});
 
   function handleFilterChange(key, value) {
     console.log(key, value);
@@ -87,35 +91,50 @@ function App() {
         ) : (
           <div className="text-center">No data</div>
         )}
-
         <div className="mt-8">
-          <form className="flex space-x-6 justify-center">
-            <Select
-              label="Year"
-              options={years}
-              value={filters.year || ""}
-              onChange={value => handleFilterChange("year", value)}
-            />
-            <Select
-              label="Quarter"
-              options={quarters}
-              value={filters.quarter || ""}
-              onChange={value => handleFilterChange("quarter", value)}
-            />
-            <Select
-              label="Home Ownership"
-              options={homeOwnerships}
-              value={filters.homeOwnership || ""}
-              onChange={value => handleFilterChange("homeOwnership", value)}
-            />
-            <Select
-              label="Term"
-              options={terms}
-              value={filters.term || ""}
-              onChange={value => handleFilterChange("term", value)}
-            />
+          <form className="">
+            <div className="flex space-x-6 justify-center">
+              <Select
+                label="Year"
+                options={years}
+                value={filters.year || ""}
+                onChange={value => handleFilterChange("year", value)}
+              />
+              <Select
+                label="Quarter"
+                options={quarters}
+                value={filters.quarter || ""}
+                onChange={value => handleFilterChange("quarter", value)}
+              />
+              <Select
+                label="Home Ownership"
+                options={homeOwnerships}
+                value={filters.homeOwnership || ""}
+                onChange={value => handleFilterChange("homeOwnership", value)}
+              />
+              <Select
+                label="Term"
+                options={terms}
+                value={filters.term || ""}
+                onChange={value => handleFilterChange("term", value)}
+              />
+            </div>
+            <div className="flex justify-center mt-8">
+              <button
+                type="reset"
+                className="flex items-center rounded-md border border-transparent bg-indigo-100 px-3 py-2 text-sm font-medium leading-4 text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                onClick={() => setFilters({})}
+              >
+                Reset
+              </button>
+            </div>
           </form>
         </div>
+        {filteredData.length > 0 && (
+          <div className="flex mt-20 justify-center">
+            <GradeBarChart gradeData={aggregatedData} />
+          </div>
+        )}
       </div>
     </div>
   );
@@ -158,6 +177,24 @@ function GradeTable({ gradeData }) {
         </div>
       </div>
     </div>
+  );
+}
+
+function GradeBarChart({ gradeData }) {
+  const transformedData = Object.entries(gradeData).map(([key, value]) => ({
+    grade: `Grade ${key}`,
+    balance: value.toFixed(2)
+  }));
+
+  return (
+    <BarChart width={730} height={250} data={transformedData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="grade" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="balance" fill="#8884d8" />
+    </BarChart>
   );
 }
 
